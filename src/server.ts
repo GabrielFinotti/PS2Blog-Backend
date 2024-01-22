4ce46d7f737988084126654e82359515d01a3e91
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-01-22 20:21:45 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,20 +3,16 @@ import express, { Request, Response } from "express";
 import connect from "../db/connect";
 import * as userController from "./modules/user-controller";
 
-// Execução do express e banco
 const app = express();
 const port = process.env.PORT || 3000;
 connect;
 
-// Permitindo recebimento de dados json
 app.use(express.json())
 
-// Controladores de Usuários
 app.use("/login", userController.loginControler);
 app.use("/register", userController.registerControler);
 app.use("/update", userController.updateControler);
 
-// Configuração da porta e verificação do estatos do servidor
 app.get("/", (req: Request, res: Response) => {
   try {
     res.status(200).send("Servidor Online");
