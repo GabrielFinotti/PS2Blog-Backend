13799165a08ba3d34b121708677f445788459a58
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-01-24 17:57:56 -0300

    API Game list
    Adicionado o método de pesquisa por nome para retorno de dados dos games

--- src/controllers/gameList/all.ts
+++ src/controllers/gameList/all.ts
@@ -4,6 +4,7 @@ import gameList from "../../models/gameList";
 export const allGames = async (req: Request, res: Response) => {
   try {
     const page = req.query.page ? parseInt(req.query.page as string) : 1;
+    const gameName = req.query.name ? req.query.name : "";
 
     if (isNaN(page)) {
       return res
@@ -11,11 +12,14 @@ export const allGames = async (req: Request, res: Response) => {
         .json({ message: "Os parâmetro de consultas devem ser números!" });
     }
 
-    const totalDocs = await gameList.countDocuments();
+    const totalDocs = await gameList.countDocuments({
+      gameName: { $regex: gameName, $options: "i" },
+    });
+
     const totalPages = Math.ceil(totalDocs / 20);
 
     const games = await gameList
-      .find()
+      .find({ gameName: { $regex: gameName, $options: "i" } })
       .skip((page - 1) * 20)
       .limit(20);
 
@@ -23,11 +27,11 @@ export const allGames = async (req: Request, res: Response) => {
     let prevPage;
 
     if (page > 1) {
-      prevPage = `?page=${page - 1}`;
+      prevPage = `?page=${page - 1}&name=${gameName}`;
     }
 
     if (page < totalPages) {
-      nextPage = `?page=${page + 1}`;
+      nextPage = `?page=${page + 1}&name=${gameName}`;
     }
 
     res.status(200).json({ games, totalPages, prevPage, nextPage });
