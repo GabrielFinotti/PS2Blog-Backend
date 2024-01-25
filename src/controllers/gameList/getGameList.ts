958b74af9fcba3d066f119d55c76fdae5624e3be
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-01-25 03:06:16 -0300

    Revert "Update getGameList.ts"
    This reverts commit 64cd0454d7e19e637892a4779094b6c332d0a2bd.

--- src/controllers/gameList/getGameList.ts
+++ src/controllers/gameList/getGameList.ts
@@ -26,6 +26,7 @@ async function gameFilter(page: number, gameName: string) {
   });
 
   const docLimit: number = 10;
+
   const totalPages = Math.ceil(totalDocs / docLimit);
 
   const games = await gameListModel
