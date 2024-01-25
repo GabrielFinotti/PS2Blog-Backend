2f7fd37140d83462c2bf29595b92848c9bb9fba9
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-01-25 02:59:51 -0300

    Dep
    Instalando biblioteca cors para configuração de cabeçalhos

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 // Importações
 import express from "express";
 import connect from "../db/connect";
+import cors from "cors";
 import { scrapingCron } from "./job/scrapingCron";
 import { router } from "./routes/routers";
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+  })
+);
 
 scrapingCron.start();
 
