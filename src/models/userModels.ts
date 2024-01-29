4c90cf33cd2063aee6cd2a972839ba21eddc66a3
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2024-01-29 00:50:18 -0300

    Revert "Update userModels.ts"
    This reverts commit 8fbb6de41c1184422954927359e65a8edc04310c.

--- src/models/userModels.ts
+++ src/models/userModels.ts
@@ -21,8 +21,6 @@ const UserSchema = new mongoose.Schema(
       required: true,
       select: false,
       trim: true,
-      minlength: 6,
-      maxlength: 20,
     },
   },
   { timestamps: true }
