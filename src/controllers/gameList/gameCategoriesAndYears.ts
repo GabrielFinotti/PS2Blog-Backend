683f4deb410ac65bb3be08d354208d3dce135025
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-07-16 13:41:15 -0300

    Refact
    Refactoring, optimization of the return code for categories and years of games!

--- src/controllers/gameList/gameCategoriesAndYears.ts
+++ src/controllers/gameList/gameCategoriesAndYears.ts
@@ -1,38 +1,22 @@
+import { categoriesAndYearsFilter } from "./../../utils/gameList/categoriesAndYearsFilter";
 import { Request, Response } from "express";
-import { gameModel } from "../../models/gameModel";
 
 export const gameCategoriesAndYears = async (req: Request, res: Response) => {
   try {
-    const categoriesAndYearsData = await gameModel
-      .find()
-      .select("category release")
-      .lean();
-
-    const categoriesAndYearsList = {
-      categories: Array.from(
-        new Set(categoriesAndYearsData.map((data) => data.category).sort())
-      ),
-      release: Array.from(
-        new Set(
-          categoriesAndYearsData
-            .map((data) => {
-              if (data.release.includes("-")) {
-                return data.release.split("-")[0];
-              } else {
-                return data.release;
-              }
-            })
-            .sort()
-        )
-      ),
-    };
+    const categoriesAndYearsList = await categoriesAndYearsFilter();
 
     return res.status(200).send(categoriesAndYearsList);
   } catch (error) {
-    console.log(`Error: ${error}`);
+    console.log(
+      `Error when trying to retrieve the categories and years of the games. Error: ${error}`
+        .red.bgBlack
+    );
 
     return res
       .status(500)
-      .send({ message: "Error when trying to retrieve categories!" });
+      .send({
+        message:
+          "Error when trying to retrieve the categories and years of the games!",
+      });
   }
 };
