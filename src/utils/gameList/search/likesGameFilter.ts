018415890c4079bdfb5ee96eb265a3ab42ae5aa3
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-07-21 14:01:22 -0300

    Fix filter
    -Fixed game filter by likes

--- src/utils/gameList/search/likesGameFilter.ts
+++ src/utils/gameList/search/likesGameFilter.ts
@@ -4,8 +4,8 @@ export const likesGameFilter = async () => {
   try {
     const gameList = await gameModel
       .find()
-      .select("image name totalLikes")
-      .sort({ totalLikes: -1 })
+      .select("image name likes.totalLikes")
+      .sort({ 'likes.totalLikes': -1 })
       .limit(5)
       .lean();
 
